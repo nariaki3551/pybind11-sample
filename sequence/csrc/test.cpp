c792169c7c35161076e77232608bf5c5842ea1b7
nariaki3551 <nariaki3551@gmail.com>
2022-10-24 11:02:32 +0900

    mv sequence/csrc/fibonacci.hpp sequence/fibonacci.hpp

--- sequence/csrc/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "fibonacci.hpp"
-
-int main() {
-   for ( int i = 1; i < 5; ++i ) {
-      std::cout << "fibonacci(" << i << ") = " << fibonacci(i) << std::endl;
-   }
-}
